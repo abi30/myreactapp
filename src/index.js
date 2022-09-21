a12b67c1fdf90d5a406a791a5dc1dc6a55dd0e2f
abi30 <rakib0751@gmail.com>
2022-09-21 17:27:36 +0200

    14.22 minit done #6

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+
+class Clock {
+    // eslint-disable-next-line class-methods-use-this
+    print() {
+        return (
+            <h1 className="heading">
+                <span className="text">Hello world {new Date().toLocaleTimeString()}</span>
+                <img src="" alt="" />
+            </h1>
+        );
+    }
+}
+const ClockComponent = new Clock();
+ClockComponent.print();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(ClockComponent.print());
+
+// export default Clock;
---
+++ src/testcode.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+const index = 0;
+
+const element = (
+    <h1 className="heading" tebIndex={index}>
+        <span className="text">Hello world {new Date().toLocaleTimeString()}</span>
+        <img src="" alt="" />
+    </h1>
+);
+
+export default element;
+
+// function Clock({ locale }) {
+//     return (
+//         <h1 className="heading">
+//             <span className="text">Hello world {new Date().toLocaleTimeString(locale)}</span>
+//             <img src="" alt="" />
+//         </h1>
+//     );
+// }
+
+<React.StrictMode>
+    //{' '}
+    <div>
+        // <Clock />
+        //{' '}
+    </div>
+    //{' '}
+</React.StrictMode>;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render();
+
+reportWebVitals();
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+
+// const index = 0;
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// setInterval(() => {
+//     const element = (
+//         <h1 className="heading" tebIndex={index}>
+//             <span className="text">Hello world {new Date().toLocaleTimeString()}</span>
+//             <img src="" alt="" />
+//         </h1>
+//     );
+//     root.render(element);
+// }, 1000);
